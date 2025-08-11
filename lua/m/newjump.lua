a9507133824434ad8e9e4144091bf1e5debd672b
TobiasMu <64594392+TobiasMu@users.noreply.github.com>
2025-08-11 18:28:21 +0200

    newjump actually works

--- lua/m/newjump.lua
+++ lua/m/newjump.lua
@@ -11,9 +11,15 @@ H.move = function(direction, config) ---@diagnostic disable-line
   if config == nil then config = H.jump_config end
   local csr = vim.api.nvim_win_get_cursor(0)
 
+  if config.searchkey ~= nil and not vim.deep_equal(csr, config.last_pos) then
+    config.searchkey = nil
+    config.last_pos = {}
+  end
+
 
   if config.searchkey == nil then
     local key = string.char(vim.fn.getchar()) ---@diagnostic disable-line 
+    config.searchkey = key
     if direction == "forward" then
       vim.api.nvim_feedkeys('/'..key .. '\r', "n", true)
     end
@@ -21,7 +27,6 @@ H.move = function(direction, config) ---@diagnostic disable-line
       vim.api.nvim_feedkeys('?'..key .. '\r', "n", true)
     end
 
-    config.searchkey = key
     vim.schedule(function()
     config.last_pos = vim.api.nvim_win_get_cursor(0)end)
     return
@@ -38,10 +43,6 @@ H.move = function(direction, config) ---@diagnostic disable-line
     config.last_pos = vim.api.nvim_win_get_cursor(0)end)
   end
 
-  if config.searchkey ~= nil and not vim.deep_equal(csr, config.last_pos) then
-    config.searchkey = nil
-    H.move(config)
-  end
   end
 vim.keymap.set("n","f",function()H.move("forward")end)
 vim.keymap.set("n","F",function()H.move("backward")end)
